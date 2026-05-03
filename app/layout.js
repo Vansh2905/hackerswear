d1476909eec1534ac51b6c97f3da8f3e3c7158ef
Vansh2905 <vanshahluwalia29@gmail.com>
2026-05-03 17:21:06 +0530

    Bug fixes

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/footer";
 import Providers from "@/components/Providers";
 import NextTopLoader from "nextjs-toploader";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Shopping site for hackers",
   description: "Hackerswear.com",
@@ -22,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={inter.className}>
         <NextTopLoader color="#22c55e" height={3} showSpinner={false} />
         <Providers>
           <Navbar />
